8d809c3d964aed6268b124896d10776f8fdf832e
Vicktor007 <vicktord007@gmail.com>
2023-06-04 11:26:03 +0100

    save

--- categories/jewelleries category/modal.js
+++ categories/jewelleries category/modal.js
@@ -1,3 +1,50 @@
+let modalContainers = document.querySelectorAll('#modal-container');
+
+for(let mod of modalContainers) {
+    mod.addEventListener('click', none2);
+}
+
+function none2(e) {
+    if(e.target === this) {
+        this.style.display = 'none';
+    }
+}
+
+let closeBtns = document.querySelectorAll('#close-btn');
+
+for(let cl of closeBtns) {
+    cl.addEventListener('click', none);
+}
+
+function none() {
+    for(let mod of modalContainers) {
+        mod.style.display = "none";
+    }
+}
+
+const openBtns = document.querySelectorAll("#open-btn");
+
+for(let op of openBtns) {
+    op.addEventListener('click', block);
+}
+
+function block() {
+    for(let mod of modalContainers) {
+        mod.style.display = 'block';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 // Variables
 
 // let openBtn = document.getElementById('open-btn');
@@ -136,38 +183,3 @@
 // });
 
 
-let modalContainers = document.querySelectorAll('#modal-container');
-
-for(let mod of modalContainers) {
-    mod.addEventListener('click', none2);
-}
-
-function none2(e) {
-    if(e.target === this) {
-        this.style.display = 'none';
-    }
-}
-
-let closeBtns = document.querySelectorAll('#close-btn');
-
-for(let cl of closeBtns) {
-    cl.addEventListener('click', none);
-}
-
-function none() {
-    for(let mod of modalContainers) {
-        mod.style.display = "none";
-    }
-}
-
-const openBtns = document.querySelectorAll("#open-btn");
-
-for(let op of openBtns) {
-    op.addEventListener('click', block);
-}
-
-function block() {
-    for(let mod of modalContainers) {
-        mod.style.display = 'block';
-    }
-}
